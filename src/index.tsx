08682130bd3a9079c969f9da40a13a8bdc22cd6f
Diego Ortiz <alejojo23@gmail.com>
2020-10-31 15:13:01 -0500

    Remove HMR for production

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import App from "./App";
 
-const render = (Component: React.FC) => {
-  return ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-render(App)
-
-if (module.hot) {
-  console.warn('HMR activated');
-  module.hot.accept('./App', () => {
-    const NextApp = require('./App').default;
-    render(NextApp);
-  });
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
