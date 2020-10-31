45bb1562d37074fd7354ae0bdac632668bf862a5
Diego Ortiz <alejojo23@gmail.com>
2020-10-30 23:41:52 -0500

    Added login styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-// import "./App.css";
+import "./App.css";
 import Login from "./containers/login/login";
 import ChatRoom from "./containers/chat-room/chat-room";
 
--- src/containers/login/login.tsx
+++ src/containers/login/login.tsx
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import './login.css'
+
 const Login = () => {
   const [userName, setUserName] = useState("");
 
   return (
-    <div>
-      <h1>Login</h1>
-      <input
-        placeholder="SuperUserName"
-        type="text"
-        onChange={(event) => setUserName(event.target.value)}
-      />
+    <div className="container">
+      <div className="login-box">
+        <h1 className="login-title">Join chat</h1>
 
-      <Link
-        onClick={(event) => (!userName ? event.preventDefault() : null)}
-        to={`/chat?username=${userName}`}
-      >
-        <button type="submit">Next</button>
-      </Link>
+        <label className="login-instruction">
+          Please enter your username
+          <input
+            className="login-input"
+            placeholder="Type your username here"
+            type="text"
+            onChange={(event) => setUserName(event.target.value)}
+          />
+        </label>
+
+        <Link
+          className="login-link"
+          onClick={(event) => (!userName ? event.preventDefault() : null)}
+          to={`/chat?username=${userName}`}
+        >
+          <button className="login-btn" type="submit">Next</button>
+        </Link>
+      </div>
     </div>
   );
 };
